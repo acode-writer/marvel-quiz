c98a74fdcb06e1d2c5a493db7cddb06dcc769293
acode-writer <souhaibou.toure122@gmail.com>
2021-06-05 15:50:57 +0000

    gestion de l'inscription.

--- src/components/Signup/index.js
+++ src/components/Signup/index.js
@@ -1,13 +1,71 @@
-import React from 'react'
-
+import { useState, useContext } from "react";
+import {FirebaseContext} from "../firebase";
 const Signup = () => {
+    const firebase = useContext(FirebaseContext);
+    const data = {
+        pseudo: "",
+        email: "",
+        password: "",
+        confirmPassword: ""
+    };
+    const [loginData, setLoginData] = useState(data);
+    const [error, setError] = useState("");
+    const onHandleChange = event => {
+        const name = event.target.id;
+        const value = event.target.value;
+        setLoginData({...loginData, [name]: value});
+    };
+
+    const onHandleSubmit = e => {
+        e.preventDefault();
+        const {pseudo, email, password} = loginData;
+        firebase.signupUser(email,password)
+        .then( user => {
+            setLoginData({...data});
+        })
+        .catch( error => {
+            setError(error);
+            setLoginData({...data});
+        });
+    }
+    const {pseudo, email, password, confirmPassword} = loginData;
+    const btn = pseudo === "" || email === "" || password === "" || password !== confirmPassword 
+    ? <button disabled className="btn-welcome">Inscription</button> : <button className="btn-welcome">Inscription</button> ;
+
+    const errorMsg = error !== "" && <span>{error.message}</span>;
+
     return (
         <div className="signUpLoginBox">
             <div className="slContainer">
-                Signup
+                <div className="formBoxLeftSignup"></div>
+                <div className="formBoxRight">
+                    <div className="formContent">
+                        {errorMsg}
+                        <h2>Inscription</h2>
+                        <form onSubmit={onHandleSubmit}>
+                            <div className="inputBox">
+                                <input type="text" id="pseudo" value={pseudo} required autoComplete="off" onChange={onHandleChange} />
+                                <label htmlFor="pseudo">Pseudo</label>
+                            </div>
+                            <div className="inputBox">
+                                <input type="email" id="email" value={email} required autoComplete="off" onChange={onHandleChange} />
+                                <label htmlFor="email">Email</label>
+                            </div>
+                            <div className="inputBox">
+                                <input type="password" id="password" value={password} required autoComplete="off" onChange={onHandleChange} />
+                                <label htmlFor="password">Mot de passe</label>
+                            </div>
+                            <div className="inputBox">
+                                <input type="password" id="confirmPassword" value={confirmPassword} required autoComplete="off" onChange={onHandleChange} />
+                                <label htmlFor="confirmPassword">Confirmer le mot de passe</label>
+                            </div>
+                            {btn}
+                        </form>
+                    </div>
+                </div>
             </div>
         </div>
-    )
-}
+    );
+};
 
 export default Signup
---
+++ src/components/firebase/firebase.js
@@ -0,0 +1,23 @@
+import app from "firebase/app";
+import "firebase/auth";
+const config = {
+    apiKey: "AIzaSyB0D0At9Kch0CvbO_NroaKwCMebUsLGifU",
+    authDomain: "marvel-quiz-5b1d3.firebaseapp.com",
+    projectId: "marvel-quiz-5b1d3",
+    storageBucket: "marvel-quiz-5b1d3.appspot.com",
+    messagingSenderId: "832734147622",
+    appId: "1:832734147622:web:71ffe4a49218d8f38922e7"
+  };
+
+export default class Firebase {
+    constructor() {
+        app.initializeApp(config);
+        this.auth = app.auth();
+    }
+
+    signupUser = (email, password) => this.auth.createUserWithEmailAndPassword(email,password);
+
+    loginUser = (email,password) => this.auth.signInWithEmailAndPassword(email,password);
+
+    signoutUser = () => this.auth.signOut();
+}
