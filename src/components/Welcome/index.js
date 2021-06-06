8922f976a10b14d2ff8a6a23cad1c6ee11aaae38
acode-writer <souhaibou.toure122@gmail.com>
2021-06-06 10:23:19 +0000

    gestion de la session sur le component Welcome

--- src/components/Welcome/index.js
+++ src/components/Welcome/index.js
@@ -1,11 +1,33 @@
-const Welcome = () => {
-    return (
+import { useContext, useState, useEffect } from "react";
+import { FirebaseContext } from "../firebase";
+import Logout from "../Logout"
+import Quiz from "../Quiz"
+
+const Welcome = props => {
+    const firebase = useContext(FirebaseContext);
+    const [userSession, setUserSession] = useState(null);
+    useEffect(() => {
+        const sessionListener = firebase.auth.onAuthStateChanged(user => {
+            user ? setUserSession(user) : props.history.push("/");
+        });
+        return () => {
+            sessionListener();
+        }
+    }, []);
+
+    return userSession === null ? (
+        <>
+            <div className="loader"></div>
+            <p className="loaderText">Loading ...</p>
+        </>
+    ) : (
         <div className="quiz-bg">
             <div className="container">
-                Welcome
+                <Logout />
+                <Quiz />
             </div>
         </div>
-    );
+    )
 };
 
 export default Welcome
