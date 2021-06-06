e7d35e87985fad8eed8e890b1ae6869f67fd22a4
acode-writer <souhaibou.toure122@gmail.com>
2021-06-06 15:41:34 +0000

    Chargement des quizz

--- src/components/Quiz/index.js
+++ src/components/Quiz/index.js
@@ -1,12 +1,52 @@
 import { Component } from "react"
-
-
+import Levels from "../Levels";
+import ProgressBar from "../ProgressBar";
+import {QuizMarvel} from "../QuizMarvel";
 export default class Quiz extends Component{
+    state = {
+        levelNames: ["debutant","confirme", "expert"],
+        quizLevel: 0,
+        maxQuestions: 10,
+        storedQuestions: [],
+        question: null,
+        options: [],
+        idQuestion: 0,
+    }
+    loadQuestion = level => {
+        const questions = QuizMarvel[0].quizz[level];
+        if(questions.length >= this.state.maxQuestions){
+            const newQuestionsArray = questions.map( ({answer, ...keepRest}) => keepRest); 
+            this.setState({...this.state, storedQuestions:newQuestionsArray});
+        }else {
+
+        }
+    };
+
+    componentDidMount() {
+        this.loadQuestion(this.state.levelNames[this.state.quizLevel]);
+    }
+
+    componentDidUpdate(prevProps,prevState) {
+        if(this.state.storedQuestions !== prevState.storedQuestions) {
+            this.setState({
+                ...this.state,
+                question: this.state.storedQuestions[this.state.idQuestion].question,
+                options: this.state.storedQuestions[this.state.idQuestion].options
+            });
+        }
+    }
+
     render() {
-        const { pseudo } = this.props.userData;
+        // const { pseudo } = this.props.userData;
+        const {question,options} = this.state;
+        const displayQuestions = options.map((option,index) => <p key={index} className="answerOptions">{option}</p>)
         return (
             <div>
-                <h2>Pseudo: {pseudo}</h2>
+                <Levels />
+                <ProgressBar />
+                <h2>{question}</h2>
+                {displayQuestions}
+                <button className="btnSubmit">Suivant</button>
             </div>
         )
     }
